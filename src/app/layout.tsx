a97e94fd6f2545581a8c9c625135529aed6a51b0
belzs <gabrielh.nehls@gmail.com>
2025-07-05 11:32:03 -0300

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,20 @@ const kanit = Kanit({
 export const metadata: Metadata = {
   title: "Gabriel H. Nehls - UX Designer",
   description: "Portfolio of Gabriel H. Nehls, UX Designer specialized in AI and WEB3",
+  icons: {
+    icon: [
+      { url: '/favicon/favicon-96x96.png', sizes: '96x96', type: 'image/png' },
+      { url: '/favicon/favicon.ico', sizes: 'any' },
+      { url: '/favicon/favicon.svg', type: 'image/svg+xml' }
+    ],
+    apple: [
+      { url: '/favicon/apple-touch-icon.png', sizes: '180x180', type: 'image/png' }
+    ],
+    other: [
+      { url: '/favicon/web-app-manifest-192x192.png', sizes: '192x192', type: 'image/png' },
+      { url: '/favicon/web-app-manifest-512x512.png', sizes: '512x512', type: 'image/png' }
+    ]
+  }
 };
 
 export default function RootLayout({
